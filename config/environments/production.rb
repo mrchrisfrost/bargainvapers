e2080547dc18c6d5a2fac41bc5aa0fafe4beff30
mrchrisfrost <androidpatmdev@gmail.com>
2014-05-04 08:47:02 -0400

    config update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.precompile += %w( vendor/modernizer.js), true 
+  config.assets.precompile += %w( vendor/modernizer.js) 
 end
